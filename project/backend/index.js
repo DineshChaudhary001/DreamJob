5246dd9f1b4b94e1bc1175a9e6d696a7117ab873
DineshChaudhary001 <dineshpatel84696@gmail.com>
2024-08-06 23:00:33 +0530

    Initial commit

---
+++ project/backend/index.js
@@ -0,0 +1,41 @@
+import express, { json } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config({});
+
+const app = express();
+app.use(json());
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+
+// api's
+app.use("/user", userRoute);
+app.use("/company", companyRoute);
+app.use("/job", jobRoute);
+app.use("/application", applicationRoute);
+
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
